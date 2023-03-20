a4294c2caa8a42b0e13648fed457454646da4ce5
Felipe Espana <faespana@espol.edu.ec>
2023-03-20 16:14:01 -0500

    perf: semana1Tarea3

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,12 @@
+import { Layout } from '../../components/layout/Layout';
 import { NextPageWithLayout } from '../page';
 
 const ProfilePage: NextPageWithLayout = () => {
   return <div>PROFILE</div>;
+};
+
+ProfilePage.getLayout = (page) => {
+  return <Layout>{page}</Layout>;
 };
 
 export default ProfilePage;
