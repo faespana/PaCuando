bfdc6c2c50c4941b3a112f053f7ab7408ad0543f
Felipe Espana <faespana@espol.edu.ec>
2023-03-20 20:47:25 -0500

    feat: semana1Tarea4

--- components/navigation/footer/Footer.tsx
+++ components/navigation/footer/Footer.tsx
@@ -1,3 +1,13 @@
 export function Footer() {
-  return <div>FOOTER</div>;
+  return (
+    <div className='min-h-[488px] flex justify-center items-center bg-[url("/footer-banner.png")] bg-cover bg-center app-banner -mt-4 gap-5'>
+      <div className="flex gap-4 mt-44">
+        <input
+          className="px-6 py-4 rounded-3xl w-full sm:w-[465px]"
+          type="text"
+          placeholder="¿Qué quieres ver en tu ciudad?"
+        />
+      </div>
+    </div>
+  );
 }
--- components/navigation/header/Header.tsx
+++ components/navigation/header/Header.tsx
@@ -1,6 +1,26 @@
+import Link from 'next/link';
+
 const Header = () => {
   return (
-    <div className="bg-black text-white flex items-center justify-between px-4 sm:px-12 py-4 min-h-[70px] text-sm"></div>
+    <div className="bg-black text-white flex items-center justify-between px-4 sm:px-12 py-4 min-h-[70px] text-sm">
+      <Link href="/">
+        <h2>PC?</h2>
+      </Link>
+
+      <div className="flex justify-between">
+        <Link href="/new_post">
+          <h3 className="mx-3.5 text-app-blue">+ Crear Publicación</h3>
+        </Link>
+
+        <Link href="/login">
+          <h3 className="mx-3.5">Log in</h3>
+        </Link>
+
+        <Link href="/sign-up">
+          <h3 className="mx-3.5">Sign up</h3>
+        </Link>
+      </div>
+    </div>
   );
 };
 
